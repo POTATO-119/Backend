eec4c283a2c025d6a3bd5ddb24f65b0040e5d775
박고운 <gounpark7475@gmail.com>
2026-01-30 18:00:35 +0900

    이미지 서버 설정 추가

--- src/main/java/com/example/potato/config/WebConfig.java
+++ src/main/java/com/example/potato/config/WebConfig.java
@@ -1,17 +1,24 @@
-//front, back 연결파일
 package com.example.potato.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173") // 주소 허용
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
     }
 }
